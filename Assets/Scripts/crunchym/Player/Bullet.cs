fdec279754a5d530d927e5901f2a379305148ede
CrunchyM <156565391+CrunchyM@users.noreply.github.com>
2024-01-14 17:38:55 -0800

    Shootin
    player is able to shoot

--- Assets/Scripts/crunchym/Player/Bullet.cs
+++ Assets/Scripts/crunchym/Player/Bullet.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public float speed = 20f;
+    public int damage = 40;
+    public Rigidbody2D rb;
+    public GameObject impactEffect;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //We are telling "RB" to move times its speed
+        rb.velocity = transform.right * speed;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    //this is our when it collieds
+    //void OnTriggerEnter2D (Collider2D hitInfo){}
         
-    }
 }
