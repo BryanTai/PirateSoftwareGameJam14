e0633a86aab79d277b810cbe497917d7650f1ffe
NBouncy <154663306+NBouncy@users.noreply.github.com>
2024-01-18 19:45:37 -0800

    Misc code stuff for enemy

---
+++ Assets/Scripts/BouncyScripts/EnemyHP.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHP : MonoBehaviour
+{
+    public int enemyhealth = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //void TakeDamage(int damage)
+        //{
+        //    health -= damage;
+
+        //    if (health <= 0)
+        //    {
+        //        Die();
+        //    }
+        //}
+
+        //void Die()
+        //{
+
+        //    Destroy(gameObject);
+        //}
+    }
+}
